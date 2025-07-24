53bdf620cd0ab639c757ccb48853675f865cc22a
snoumaz <dablosk@proton.me>
2025-07-24 16:39:57 +0200

    Refactor app.js para agregar conexión a la base de datos y crear tabla de mensajes; actualizar Dockerfile para mejorar la estructura del proyecto.

--- Express/express_07/app.js
+++ Express/express_07/app.js
@@ -3,16 +3,38 @@ import logger from 'morgan'
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
 process.loadEnvFile()
+import { createClient } from '@libsql/client'
+
+const db = createClient({
+    url: process.env.DB_URL,
+    authToken: process.env.DB_TOKEN
+})
+
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id_message INTEGER PRIMARY KEY AUTOINCREMENT,
+        user TEXT default 'Anonimo',
+        content TEXT,
+        fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )`)       
+
 const PORT = process.env.PORT || 10000
 const app = express()
 const server = createServer(app)
 
+const io = new Server(server, {
+    connectionStateRecovery: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+})
 app.use(logger('dev'))
 app.use(express.static(process.cwd() + "/client"))
 
 app.get("/",(req,res) => {
     res.sendFile(process.cwd() + "/client/index.html")
 })
+
 
 server.listen(PORT,()=>{
     console.log(`Servidor iniciado en http://localhost:${PORT}`);
